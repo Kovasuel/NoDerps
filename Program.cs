e1cb31cafbe7d488625b8b5d26dd5cea3cc4e8af
Kovasuel <eddieguinn@gmail.com>
2018-11-13 02:32:55 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace NoDerps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Build().Run();
+        }
+
+        public static IWebHostBuilder BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+
+                // params string[] urls
+                .UseUrls(urls: "https://10.142.0.2:5001");
+
+                //.Build();
+    }
+}
