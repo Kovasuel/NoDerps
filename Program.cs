da19d6cadc09a97fc0f58ba1f8e8d59cfec22e2c
Kovasuel <eddieguinn@gmail.com>
2018-11-24 15:08:54 -0500

    changed to port 5000 since Apache should be handling the 5001 traffic...

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace NoDerps
                 // params string[] urls
                 //Was previously specific: .UseUrls(urls: "https://10.142.0.3:5001");
                     //Set to use all on the port so that it will all get fed into the rev. proxy.
-                    .UseUrls(urls: "https://*:5001")
+                    .UseUrls(urls: "https://*:5000")
                 .UseStartup<Startup>();
                 //.Build();
     }
